56828a7fb0ebd0be893638c1a180a9cb3b5433c3
ajh334 <ajh334@drexel.edu>
2015-02-21 20:56:25 -0500

    Player Controls
    Added player where the turret can rotate and can fire test shots. Player
    and test shot has been added as prefabs inside the project I created
    named Project

---
+++ Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerController : MonoBehaviour {
+
+
+	public float rotationSpeed = 80f;
+
+	private float rotateValue = 0f;
+
+	protected Vector3 move =  Vector3.zero;
+
+
+
+
+	// Update is called once per frame
+
+	void Start()
+	{
+
+	}
+	void Update () {
+
+		rotateValue -= Input.GetAxis ("Horizontal") * rotationSpeed * Time.deltaTime;
+		rotateValue = Mathf.Clamp (rotateValue, -30, 30);
+		transform.localEulerAngles = new Vector3 (0f, 0f,rotateValue);
+
+
+	}
+}
