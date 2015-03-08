12e62f72c47738fef0fd37103cd6abc5b523d937
ajh334 <ajh334@drexel.edu>
2015-03-07 20:27:12 -0500

    asteroid_movement_done
    added asteroid movement script. Have clockwise set to 1 for clockwise
    movement, anytthing else for counter clockwise. Rotation value zero
    means no rotation.

---
+++ Project/Assets/AsteroidMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidMovement : MonoBehaviour {
+
+	public float rotateValue = 0f;
+	public float rotateClockwise = 1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (rotateClockwise == 1f) {
+			transform.Rotate(0f, 0f, rotateValue * Time.deltaTime);
+		} else {
+			transform.Rotate(0f, 0f, rotateValue * Time.deltaTime);
+		}
+
+	}
+}
